1b39f26cc1bd7bbc889ff08e761bc0d0cf16678f
Utkarsh Patil <utkarshpatil191199@gmail.com>
2024-10-20 17:54:29 +0530

    single linked list updated

--- LinkedList/single_linked_list.cpp
+++ LinkedList/single_linked_list.cpp
@@ -1,5 +1,6 @@
 /* 1. Program of single linked list */
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -16,12 +17,137 @@ public:
     }
 };
 
+class LinkedList
+{
+private:
+    Node *head;
+
+public:
+    LinkedList()
+    {
+        head = nullptr;
+    }
+
+    void CreateList()
+
+    {
+        int number_of_node, data = 0;
+
+        cout << "Enter the number of nodes : ";
+        cin >> number_of_node;
+
+        if (number_of_node == 0)
+            return;
+
+        cout << "Please enter the 1 element to be inserted: ";
+        cin >> data;
+
+        AddAtBeginning(data);
+
+        for (int i = 2; i <= number_of_node; i++)
+        {
+            cout << "Please enter the " << i << " element to be inserted: ";
+            cin >> data;
+
+            AddAtEnd(data);
+        }
+    }
+
+    void Display() const
+    {
+        if (head == nullptr)
+        {
+            cout << "List is empty!!!" << endl;
+            return;
+        }
+
+        cout << "List: ";
+
+        Node *temp = head;
+
+        while (temp != nullptr)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+
+        cout << "NULL" << endl;
+    }
+
+    int Count() const
+    {
+        int count = 0;
+
+        Node *temp = head;
+
+        while (temp != nullptr)
+        {
+            temp = temp->next;
+            count++;
+        }
+
+        return count;
+    }
+
+    bool Search(int search) const
+    {
+        Node *temp = head;
+
+        while (temp != nullptr)
+        {
+            if (temp->data == search)
+                return true;
+
+            temp = temp->next;
+        }
+
+        return false;
+    }
+
+    void AddAtBeginning(int value)
+    {
+        Node *new_node = new Node(value);
+        new_node->next = head;
+        head = new_node;
+    }
+
+    void AddAtEnd(int value)
+    {
+        if (head == nullptr)
+        {
+            AddAtBeginning(value);
+            return;
+        }
+
+        Node *temp = head;
+
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+
+        if (temp != nullptr)
+        {
+            Node *new_node = new Node(value);
+            temp->next = new_node;
+        }
+    }
+
+    void AddAfterNode(int value, int item)
+    {
+        if(head){}
+    }
+};
+
 int main()
 {
-    int choice, data, item, pos;
+    int choice, data, item, pos, search;
+    LinkedList linked_list;
 
     while (1)
     {
+        system("cls");
+
         cout << "1. Create empty list" << endl;
         cout << "2. Display" << endl;
         cout << "3. Count" << endl;
@@ -40,6 +166,44 @@ int main()
 
         switch (choice)
         {
+        case 1:
+            linked_list.CreateList();
+            break;
+
+        case 2:
+            linked_list.Display();
+            break;
+
+        case 3:
+            cout << "Total number of nodes: " << linked_list.Count() << endl;
+            break;
+
+        case 4:
+            cout << "Enter element to search: ";
+            cin >> search;
+            cout << (linked_list.Search(search) ? "Element found" : "Element not found") << endl;
+            break;
+
+        case 5:
+            cout << "Enter the element to add at beginning: ";
+            cin >> data;
+            linked_list.AddAtBeginning(data);
+            break;
+
+        case 6:
+            cout << "Enter the element to add at end: ";
+            cin >> data;
+            linked_list.AddAtEnd(data);
+            break;
+
+        case 7:
+            cout << "Enter the element to add: ";
+            cin >> data;
+            cout << "Enter the node after which to add: ";
+            cin >> item;
+            linked_list.AddAfterNode(data, item);
+            break;
+
         case 12:
             exit(1);
             break;
